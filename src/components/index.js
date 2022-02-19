0816fcd4aa1c6d6b3babacabd8f0873b2f96f115
Georgeking97 <44296390+Georgeking97@users.noreply.github.com>
2022-02-19 17:46:28 +0000

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export {default as Article } from './article/Article';
+export {default as Brand } from './brand/Brand';
+export {default as CTA } from './cta/CTA';
+export {default as Feature } from './feature/Feature';
+export {default as Navbar } from './navbar/Navbar';
+
+/* this file allows me to export all components from a single file and not bulk up my other files with them all */
---
+++ src/containers/index.js
@@ -0,0 +1,8 @@
+export { default as Blog } from './blog/Blog'
+export { default as Features } from './features/Features'
+export { default as Footer } from './footer/Footer'
+export { default as Header } from './header/Header'
+export { default as Possibility } from './possibility/Possibility'
+export { default as WhatGPT3 } from './whatGPT3/WhatGPT3'
+
+/* this file allows me to export all containers from a single file and not bulk up my other files with them all */
